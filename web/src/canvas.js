f4694d3259973c46b5e45c19ef3d56940a113edb
baku89 <mail@baku89.com>
2016-11-11 17:25:13 -0500

    [web] Add terrain squares in 3d space

--- web/src/canvas.js
+++ web/src/canvas.js
@@ -1,10 +1,27 @@
+import VirtualScroll from 'virtual-scroll'
+
+import LerpSoft from './lerp-soft'
+
+const LAND_STEP = 30
+const LAND_SIZE = 50
+
 export default class Canvas {
 
 	constructor() {
+		this.lerps = {}
+
+		this.numLand = 50
+
 		this.initScene()
 
+		this.virtualScroll = new VirtualScroll()
+		this.virtualScroll.on(this.onScroll.bind(this))
+
+
+		// add event
 		$(window).on('resize', this.resizeCanvas.bind(this))
 
+		// start frame
 		this.render = this.render.bind(this)
 		this.render()
 	}
@@ -12,21 +29,40 @@ export default class Canvas {
 	initScene() {
 		this.scene = new THREE.Scene()
 
-		this.camera = new THREE.PerspectiveCamera(75, 1, .1, 1000)
-		this.camera.position.set(100, 100, 100)
+		this.cameraParent = new THREE.Object3D()
+		// this.cameraParent.rotation.y = -Math.PI * .5
+		this.scene.add(this.cameraParent)
+
+		this.lerpScroll = new LerpSoft(0, {
+			coeff: .1,
+			max: 0,
+			min: -50 * LAND_STEP
+		})
+
+		this.camera = new THREE.PerspectiveCamera(30, 1, .1, 1000)
+		this.camera.position.set(0, 80, 150)
 		this.camera.lookAt(new THREE.Vector3(0, 0, 0))
+
+		this.cameraParent.add(this.camera)
 
 		this.renderer = new THREE.WebGLRenderer({
 			canvas: $('.canvas')[0]
 		})
-		this.renderer.setClearColor(0x00ff00)
+		this.renderer.setClearColor(0x222266)
 
 		{
 			// debug
-			let geom = new THREE.BoxGeometry(10, 10, 10)
-			let mat = new THREE.MeshBasicMaterial({color: 0xff0000})
-			let mesh = new THREE.Mesh(geom, mat)
-			this.scene.add(mesh)
+			let geom = new THREE.PlaneGeometry(LAND_SIZE, LAND_SIZE, 5, 5)
+			let mat = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true})
+
+			for (let i = 0; i < 50; i++) {
+				let mesh = new THREE.Mesh(geom, mat)
+				mesh.position.y = i * -LAND_STEP
+				mesh.position.z = i * LAND_SIZE
+				mesh.rotation.x = Math.PI / 2
+				this.scene.add(mesh)
+			}
+
 		}
 
 		this.resizeCanvas()
@@ -35,11 +71,22 @@ export default class Canvas {
 	resizeCanvas() {
 		this.renderer.setSize(window.innerWidth, window.innerHeight)
 		this.camera.aspect = window.innerWidth / window.innerHeight
+		this.camera.updateProjectionMatrix()
+	}
+
+	onScroll(e) {
+		this.lerpScroll.offsetTarget(e.deltaY / 20)
 	}
 
 	render() {
-		console.log('a')
 		requestAnimationFrame(this.render)
+
+		this.lerpScroll.update()
+		this.cameraParent.position.y = this.lerpScroll.value
+		this.cameraParent.position.z = this.lerpScroll.value * -(LAND_SIZE / LAND_STEP)
+
+		console.log(this.lerpScroll.value)
+
 		this.renderer.render(this.scene, this.camera)
 	}
 }
---
+++ web/src/lerp-soft.js
@@ -0,0 +1,33 @@
+export default class LerpSoft {
+
+	constructor(initValue, params = {}) {
+
+		this.coeff = params.coeff || .5
+		this.max = params.max
+		this.min = params.min
+
+		console.log(this.max, this.min)
+
+		this.value = initValue
+		this.target = initValue
+	}
+
+	setTarget(target) {
+		if (target > this.max) {
+			this.target = this.max
+		} else if (target < this.min) {
+			this.target = this.min
+		} else {
+			this.target = target
+		}
+	}
+
+	offsetTarget(delta) {
+		this.setTarget(this.target + delta)
+	}
+
+	update() {
+		this.value += (this.target - this.value) * this.coeff
+	}
+
+}
